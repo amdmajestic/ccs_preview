c5a7ca88d4d87ca65dfef395bcff9c836fd57f28
amdmajestic <148652240+amdmajestic@users.noreply.github.com>
2025-02-11 21:45:18 +0500

    Setting up 'media' and 'static' URLs.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import InstructorRegisterView, InstructorLoginView, InstructorProfileView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", lambda request: redirect("instructor_login")),
+
     path('instructors/register/', InstructorRegisterView.as_view(), name='instructor_register'),
     path('instructors/login/', InstructorLoginView.as_view(), name='instructor_login'),
     path('instructors/profile/<int:pk>/', InstructorProfileView.as_view(), name='instructor_profile'),
 ]
 
+"""Including Media URLs"""
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("", lambda request: redirect("api/")),
@@ -32,6 +30,3 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("course_allocation/", include("course_allocation.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
