0bc8e3582dea4056fc5f639406b3dd72f3ae5da4
amdmajestic <148652240+amdmajestic@users.noreply.github.com>
2025-02-18 03:11:01 +0500

    FIXED

--- frontend/src/assets/__assets_traits.js
+++
@@ -1,7 +0,0 @@
-import CcsLogo from './media/ccs-logo.png';
-
-const _ASSETS_ = {
-    CcsLogo,
-}
-
-export default _ASSETS_;
--- frontend/stringExtensions.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts a string to sentence case.
- * @returns {string} The string in sentence case.
- */
-interface String {
-    toSentenceCase(): string;
-    toTitleCase(): string;
-    stringToBool(): boolean;
-}
